815b3da674afdffdb3a9f97e9ade39d693a71f34
aaronsheah <aaronsheah@gmail.com>
2013-11-27 15:30:34 +0000

    added constants to BamocarRegisters.h

--- CAN_Logger/BamocarRegisters.h
+++ CAN_Logger/BamocarRegisters.h
@@ -1,6 +1,10 @@
 #ifndef BamocarRegisters_h
 #define BamocarRegisters_h
 
-#define REG_N_ACTUAL 0x30
+#define REG_N_ACTUAL 	0x30		//RPM
+#define REG_TORQUE_CMD 	0x90		//Torque reference
+#define REG_TEMP_MOTOR	0x49		//Active motor temperature
+#define	REG_TEMP_IGBT	0x4A		//Active output stage temperature
+#define	REG_TEMP_AIR	0x48		//Current air temperature in the servo
 
 #endif
