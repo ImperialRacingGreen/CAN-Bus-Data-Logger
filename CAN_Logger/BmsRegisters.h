ed49b973b834bdf1d5ec0bb826da78b07c1e7632
aaronsheah <aaronsheah@gmail.com>
2014-01-27 22:35:03 +0000

    BMS Registers
    in .h file and excel spreadsheet

---
+++ CAN_Logger/BmsRegisters.h
@@ -0,0 +1,11 @@
+#ifdef BmsRegisters_h
+#define BmsRegisters_h
+
+#define REG_VOLTAGE		0x46
+#define REG_AVGTEMP		0x48
+#define REG_MAXTEMP		0x49
+#define REG_SOC			0x50
+#define REG_CCL			0x64
+#define REG_DCL			0x65
+
+#endif
